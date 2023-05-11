7b008ab8e7f3de0a04d3a0fa7a6dc972dd8c4648
ujjal <ujjalkumarmaity1998@gmail.com>
2023-05-11 12:40:25 +0530

    new file structure

--- profiler/main/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Accountusers(models.Model):
-    username = models.CharField(max_length=300)
-    email = models.EmailField(max_length=300)
-    password = models.CharField(max_length=100)
-    
